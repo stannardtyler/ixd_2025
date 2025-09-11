22df9422183f9c431b0ecb2e0d73ea0906c3fcd3
stannardtyler <tstannard64@gmail.com>
2025-09-11 12:33:32 -0700

    Delete eloquentJS directory

--- eloquentJS/2_0/answer.js
+++
@@ -1,3 +0,0 @@
-for (let word = "#"; word.length < 8; word += "#") {
-  console.log(word);
-}
--- eloquentJS/2_0/result.js
+++
@@ -1,8 +0,0 @@
-let wordcounter = 0;
-let word = "#";
-
-while (wordcounter < 7) {
-  console.log(word);
-  word = word + "#";
-  wordcounter = wordcounter + 1;
-}
